0a30618512b7fed86e8bddb14d745d22bebd4bd9
Peter Adelmann <peter.adelmann@trilobit.de>
2022-01-19 15:16:24 +0100

    improve and or add news and events support

--- src/EventListener/NewsarchiveLabelCallbackListener.php
+++ src/EventListener/NewsarchiveLabelCallbackListener.php
@@ -11,32 +11,36 @@ declare(strict_types=1);
 
 namespace Trilobit\AceidBundle\EventListener;
 
+use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\CoreBundle\ServiceAnnotation\Callback;
 use Contao\Database;
 use Contao\DataContainer;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
- * @Callback(table="tl_calendar", target="list.label.label")
+ * @Callback(table="tl_news_archive", target="list.label.label")
  */
-class CalendarLabelCallbackListener
+class NewsarchiveLabelCallbackListener
 {
     private $translator;
+    private $framework;
 
-    public function __construct(?TranslatorInterface $translator)
+    public function __construct(ContaoFramework $framework, ?TranslatorInterface $translator)
     {
+        $this->framework = $framework;
         $this->translator = $translator;
     }
 
     public function __invoke(array $row, string $label, DataContainer $dc, array $labels): string
     {
-        return $label.' <span style="color: #A3A3A3; padding-left: 3px;">[ID: '.$row['id'].' / '.$this->translator->trans('MSC.filterRecords', [], 'contao_default').': '.self::getChildRecordsCount($row['id']).']';
+        return $label
+            .'<span style="color:#A3A3A3;margin-left:3px;padding-left:3px">[ID: '.$row['id'].' / '.$this->translator->trans('MSC.filterRecords', [], 'contao_default').': '.self::getChildRecordsCount($row['id']).']</span>';
     }
 
     protected static function getChildRecordsCount($pid): string
     {
         return Database::getInstance()
-            ->prepare('SELECT count(id) AS count FROM tl_calendar_events WHERE pid=?')
+            ->prepare('SELECT count(id) AS count FROM tl_news WHERE pid=?')
             ->execute($pid)->count;
     }
 }
