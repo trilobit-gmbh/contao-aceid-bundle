4d89696ee12a73f25391cca7aa45d5379ea5cb0f
Peter Adelmann <peter.adelmann@trilobit.de>
2022-03-10 20:43:05 +0100

    php8 issues

--- src/EventListener/ArticleLabelCallbackListener.php
+++ src/EventListener/ArticleLabelCallbackListener.php
@@ -65,11 +65,19 @@ class ArticleLabelCallbackListener
         foreach (self::getChildRecords($row['id'], $this->ptable) as $option) {
             $childs[] = '&rarr; '
                 .'<a href="contao?do=article&table=tl_content&id='.$option['id'].'&amp;popup=1&amp;nb=1&amp;act=edit&amp;rt='.REQUEST_TOKEN.'" title="'.sprintf($this->translator->trans('tl_content.edit', [], 'contao_default'), $option['id']).'" class="edit" onclick="Backend.openModalIframe({\'title\':\''.StringUtil::specialchars(str_replace("'", "\\'", 'ID: '.$option['id'])).'\',\'url\':this.href});return false">'
-                .'<span style="color:'.(false === strpos($option['id'], $filterId) ? '#A3A3A3' : '#444').'">'
-                .(!empty($filterId) && $filterId === $option['id'] ? '<span style="font-weight:bold">' : '')
+                .'<span style="color:'
+                .(!empty($filterId) && false !== strpos((string) $option['id'], $filterId)
+                    ? '#444'
+                    : '#A3A3A3'
+                )
+                .'">'
                 .$this->translator->trans('CTE.'.$option['type'].'.0', [], 'contao_default')
-                .' <span style="color:#A3A3A3;padding:0 12px 0 3px">[ID: '.str_replace($filterId, '<span style="font-weight:bold;color:#444">'.$filterId.'</span>', $option['id']).']</span>'
-                .(!empty($filterId) && $filterId === $option['id'] ? '</span>' : '')
+                .' <span style="color:#A3A3A3;padding:0 12px 0 3px">[ID: '
+                .(!empty($filterId)
+                    ? str_replace($filterId, '<span style="color:#444;font-weight:bold">'.$filterId.'</span>', (string) $option['id'])
+                    : $option['id']
+                )
+                .']</span>'
                 .Image::getHtml('edit.svg', sprintf($this->translator->trans('tl_content.edit', [], 'contao_default'), $option['id']), 'style="margin-bottom:2px"')
                 .'</span>'
                 .'</a>'
