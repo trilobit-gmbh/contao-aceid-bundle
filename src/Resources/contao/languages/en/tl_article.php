18fd56856a3c3700f5d1883f4894f33f106cb226
Peter Adelmann <peter.adelmann@trilobit.de>
2024-06-27 11:06:24 +0200

    add translation

---
+++ src/Resources/contao/languages/en/tl_article.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  trilobit GmbH
+ * @author     trilobit GmbH <https://github.com/trilobit-gmbh>
+ * @license    LGPL-3.0-or-later
+ */
+
+$GLOBALS['TL_LANG']['tl_article']['contentElements'][0] = 'Content elements';
+$GLOBALS['TL_LANG']['tl_article']['contentElements'][1] = 'IDs of the content elements linked to this article.';
---
+++ src/Resources/contao/languages/en/tl_maintenance.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  trilobit GmbH
+ * @author     trilobit GmbH <https://github.com/trilobit-gmbh>
+ * @license    LGPL-3.0-or-later
+ */
+
+$GLOBALS['TL_LANG']['tl_maintenance']['aceid']['headline'] = 'Assign article content elements';
+$GLOBALS['TL_LANG']['tl_maintenance']['aceid']['description'] = 'Aceid, the extension that makes the content element ID in articles visible and searchable, must be initialized so that all content elements can be found.';
+$GLOBALS['TL_LANG']['tl_maintenance']['aceid']['submit'] = 'Initialize now';
+$GLOBALS['TL_LANG']['tl_maintenance']['aceid']['success'] = 'The content elements of %s articles were successfully mapped for search.';
+$GLOBALS['TL_LANG']['tl_maintenance']['aceid']['error'] = 'We could not find any products.';
