8a1769c70f3946c1fb7a6785c073aaa7fb721c33
Peter Adelmann <peter.adelmann@trilobit.de>
2024-06-27 11:24:07 +0200

    fix typo

--- src/Resources/contao/languages/en/tl_maintenance.php
+++ src/Resources/contao/languages/en/tl_maintenance.php
@@ -12,4 +12,4 @@ $GLOBALS['TL_LANG']['tl_maintenance']['aceid']['headline'] = 'Assign article con
 $GLOBALS['TL_LANG']['tl_maintenance']['aceid']['description'] = 'Aceid, the extension that makes the content element ID in articles visible and searchable, must be initialized so that all content elements can be found.';
 $GLOBALS['TL_LANG']['tl_maintenance']['aceid']['submit'] = 'Initialize now';
 $GLOBALS['TL_LANG']['tl_maintenance']['aceid']['success'] = 'The content elements of %s articles were successfully mapped for search.';
-$GLOBALS['TL_LANG']['tl_maintenance']['aceid']['error'] = 'We could not find any products.';
+$GLOBALS['TL_LANG']['tl_maintenance']['aceid']['error'] = 'We could not find any articles.';
