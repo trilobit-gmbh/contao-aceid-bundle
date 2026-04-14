89a6af8975041ea3c8f475427cde19579fa117c2
Peter Adelmann <peter.adelmann@trilobit.de>
2026-04-14 12:00:15 +0200

    cr feedback

--- src/Resources/contao/languages/de/tl_maintenance.php
+++ src/Resources/contao/languages/de/tl_maintenance.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  */
 
 $GLOBALS['TL_LANG']['tl_maintenance']['aceid']['headline'] = 'Artikel-Inhaltselemente zuordnen <span class="tl_gray">[contao-aceid-bundle]</span>';
-$GLOBALS['TL_LANG']['tl_maintenance']['aceid']['description'] = 'Die Erweiterung muss initialisiert werden, damit die Inhaltselements-ID`s in der Artikel-Übersicht sicht- und durchsuchbar werden.';
+$GLOBALS['TL_LANG']['tl_maintenance']['aceid']['description'] = 'Die Erweiterung muss initialisiert werden, damit die Inhaltselements-IDs in der Artikel-Übersicht sicht- und durchsuchbar werden.';
 $GLOBALS['TL_LANG']['tl_maintenance']['aceid']['submit'] = 'Jetzt initialisieren';
 $GLOBALS['TL_LANG']['tl_maintenance']['aceid']['success'] = 'Die Inhaltselemente von %s Artikeln wurden erfolgreich für die Suche zugeordnet.';
 $GLOBALS['TL_LANG']['tl_maintenance']['aceid']['error'] = 'Es konnten keine Artikel gefunden werden.';
